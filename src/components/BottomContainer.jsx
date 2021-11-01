055c26f52eaf6d2c27124db83cad516c2f090299
miruoo <42486288+miily8310s@users.noreply.github.com>
2021-11-01 09:04:51 +0900

    feat/container apply main container styles (#1)

--- src/components/BottomContainer.jsx
+++ src/components/BottomContainer.jsx
@@ -1,3 +1,15 @@
 export const BottomContainer = () => {
-  return <div></div>;
+  return (
+    <div class="page-bottom">
+      <div class="bottom-container">
+        <p>
+          <strong>ページ下部コンテナ</strong>
+        </p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum,
+        aspernatur fuga iusto debitis eaque eius provident libero suscipit quam!
+        Suscipit amet dignissimos id soluta quae, veniam fuga consequuntur
+        placeat magni!
+      </div>
+    </div>
+  );
 };
