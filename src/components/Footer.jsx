bc43e73a516b383bfaa55d1763a723c8f4ede2a0
miruoon <rillakerika@gmail.com>
2021-11-04 21:45:39 +0900

    feat/footer apply ch5-8 styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,31 @@ export const Footer = () => {
             <img src={footerLogo} alt="Tansaku!" />
           </a>
         </div>
+        <ul className="footer-pages-nav">
+          <li>
+            <a href="/">HOME</a>
+          </li>
+          <li>
+            <a href="/">最新の特集</a>
+          </li>
+          <li>
+            <a href="/">地域別に読む</a>
+          </li>
+          <li>
+            <a href="/">連載一覧</a>
+          </li>
+          <li>
+            <a href="/">インタビュー</a>
+          </li>
+        </ul>
+        <ul className="footer-pages-nav -about">
+          <li>
+            <a href="/">このサイトについて</a>
+          </li>
+          <li>
+            <a href="/">運営会社</a>
+          </li>
+        </ul>
         <p className="copyright">&copy; Tansaku! All rights reserved.</p>
       </div>
     </footer>
