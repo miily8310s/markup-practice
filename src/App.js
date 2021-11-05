44f39261afd3c3f323c230d21e0891515af56afa
miruoon <rillakerika@gmail.com>
2021-11-05 21:14:42 +0900

    feat/side-page apply chap7-1 styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles/normalize.css";
 import { Routes, Route } from "react-router-dom";
 import { Post } from "./pages/Post";
+import { Side } from "./pages/Side";
 import { Home } from "./pages/Home";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/post" element={<Post />} />
+      <Route path="/side" element={<Side />} />
     </Routes>
   );
 }
---
+++ src/components/SidebarContainer.jsx
@@ -0,0 +1,3 @@
+export const SidebarContainer = () => {
+  return <aside className="columns-side">aaaaaaaaaaaaaaaaa</aside>;
+};
