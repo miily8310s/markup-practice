0e89950a09d1e4eaae17c48c7ec74d4d48dc8717
miruoo <42486288+miily8310s@users.noreply.github.com>
2021-11-06 08:34:58 +0900

    feat/form apply chap8 styles (#7)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles/normalize.css";
 import { Routes, Route } from "react-router-dom";
 import { Post } from "./pages/Post";
 import { Side } from "./pages/Side";
+import { Form } from "./pages/Form";
 import { Home } from "./pages/Home";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/post" element={<Post />} />
       <Route path="/side" element={<Side />} />
+      <Route path="/form" element={<Form />} />
     </Routes>
   );
 }
